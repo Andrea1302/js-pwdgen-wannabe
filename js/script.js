06b6585d68a3b0106b718053f327e9d220f1a929
Andrea Scarpa <scarpandrea.1996@hotmail.com>
2021-10-20 16:24:29 +0200

    Pulizia js

--- js/script.js
+++ js/script.js
@@ -5,23 +5,23 @@ Infine scrivi sulla pagina “La password assegnata è: ” nomecognomecolorepre
 
 
     // Chiediamo all utente il suo nome 
-    let nome = prompt('Inserisci il tuo nome');
-    console.log(nome);
+        let nome = prompt('Inserisci il tuo nome');
+        console.log(nome);
     // chiediamo all utente il suo cognome 
-    let cognome = prompt('Inserisci il tuo cognome');
-    console.log(cognome);
+        let cognome = prompt('Inserisci il tuo cognome');
+        console.log(cognome);
 
     // Chiediamo all utente il suo colore preferito
-    let colore_preferito = prompt ('Quale è il tuo colore preferito?');
-    console.log(colore_preferito);
+        let colore_preferito = prompt ('Quale è il tuo colore preferito?');
+        console.log(colore_preferito);
     // definiamo un valore ipotetico (finale anno)
-    const finale_anno =('21')
+        const finale_anno =('21');
     // Generazione password 
-    let genera_password= `${nome}${cognome}${colore_preferito}${finale_anno}`
-    console.log(
-        `${nome}${cognome}${colore_preferito}${finale_anno}`
-        );
+        let genera_password= `${nome}${cognome}${colore_preferito}${finale_anno}`
+        console.log(
+            `${nome}${cognome}${colore_preferito}${finale_anno}`
+            );
 
     // Restituiamo all utente una possibile passsword generata 
-    document.getElementById('ms_password').innerHTML = `La password generata per te &egrave: ${genera_password}` ;
+        document.getElementById('ms_password').innerHTML = `La password generata per te &egrave: ${genera_password}` ;
     
