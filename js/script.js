6cf939d22bbb01fd56e40e99871df129d491daae
Andrea Scarpa <scarpandrea.1996@hotmail.com>
2021-10-20 16:33:38 +0200

    Commentati console log

--- js/script.js
+++ js/script.js
@@ -6,21 +6,21 @@ Infine scrivi sulla pagina “La password assegnata è: ” nomecognomecolorepre
 
     // Chiediamo all utente il suo nome 
         let nome = prompt('Inserisci il tuo nome');
-        console.log(nome);
+        // console.log(nome);
     // chiediamo all utente il suo cognome 
         let cognome = prompt('Inserisci il tuo cognome');
-        console.log(cognome);
+        // console.log(cognome);
 
     // Chiediamo all utente il suo colore preferito
         let colore_preferito = prompt ('Quale è il tuo colore preferito?');
-        console.log(colore_preferito);
+        // console.log(colore_preferito);
     // definiamo un valore ipotetico (finale anno)
         const finale_anno =('21');
     // Generazione password 
         let genera_password= `${nome}${cognome}${colore_preferito}${finale_anno}`;
-        console.log(
+        /* console.log(
             `${nome}${cognome}${colore_preferito}${finale_anno}`
-            );
+           );*/
 
     // Restituiamo all utente la password generata 
         document.getElementById('ms_password').innerHTML = `La password generata per te &egrave: ${genera_password}` ;
