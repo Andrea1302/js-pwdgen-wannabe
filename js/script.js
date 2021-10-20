86b254004ee282a7e30075d644f43784c9a2b279
Andrea Scarpa <scarpandrea.1996@hotmail.com>
2021-10-20 16:47:19 +0200

    Corretta indentazione js

--- js/script.js
+++ js/script.js
@@ -4,27 +4,27 @@
 Infine scrivi sulla pagina “La password assegnata è: ” nomecognomecolorepreferito21  */
 
 
-    // Chiediamo all utente il suo nome 
-        let nome = prompt('Inserisci il tuo nome');
-        // console.log(nome);
+// Chiediamo all utente il suo nome 
+let nome = prompt('Inserisci il tuo nome');
+// console.log(nome);
 
-    // chiediamo all utente il suo cognome 
-        let cognome = prompt('Inserisci il tuo cognome');
-        // console.log(cognome);
+// chiediamo all utente il suo cognome 
+let cognome = prompt('Inserisci il tuo cognome');
+// console.log(cognome);
 
-    // Chiediamo all utente il suo colore preferito
-        let colore_preferito = prompt ('Quale è il tuo colore preferito?');
-        // console.log(colore_preferito);
+// Chiediamo all utente il suo colore preferito
+let colore_preferito = prompt ('Quale è il tuo colore preferito?');
+// console.log(colore_preferito);
 
-    // definiamo un valore ipotetico (finale anno)
-        const finale_anno =('21');
+// definiamo un valore ipotetico (finale anno)
+const finale_anno =('21');
 
-    // Generazione password 
-        let genera_password= (`${nome}${cognome}${colore_preferito}${finale_anno}`);
-        /* console.log(
-            `${nome}${cognome}${colore_preferito}${finale_anno}`
-           );*/
+// Generazione password 
+let genera_password= (`${nome}${cognome}${colore_preferito}${finale_anno}`);
+/* console.log(
+`${nome}${cognome}${colore_preferito}${finale_anno}`
+);*/
 
-    // Restituiamo all utente la password generata 
-        document.getElementById('ms_password').innerHTML = `La password generata per te &egrave: ${genera_password}` ;
+// Restituiamo all utente la password generata 
+document.getElementById('ms_password').innerHTML = `La password generata per te &egrave: ${genera_password}` ;
     
