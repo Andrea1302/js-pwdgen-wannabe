6d464935afc531cec0fd877e22e04999781562ce
Andrea Scarpa <scarpandrea.1996@hotmail.com>
2021-10-20 16:34:12 +0200

    Pulizia js con spazi

--- js/script.js
+++ js/script.js
@@ -7,6 +7,7 @@ Infine scrivi sulla pagina “La password assegnata è: ” nomecognomecolorepre
     // Chiediamo all utente il suo nome 
         let nome = prompt('Inserisci il tuo nome');
         // console.log(nome);
+        
     // chiediamo all utente il suo cognome 
         let cognome = prompt('Inserisci il tuo cognome');
         // console.log(cognome);
@@ -14,8 +15,10 @@ Infine scrivi sulla pagina “La password assegnata è: ” nomecognomecolorepre
     // Chiediamo all utente il suo colore preferito
         let colore_preferito = prompt ('Quale è il tuo colore preferito?');
         // console.log(colore_preferito);
+
     // definiamo un valore ipotetico (finale anno)
         const finale_anno =('21');
+
     // Generazione password 
         let genera_password= `${nome}${cognome}${colore_preferito}${finale_anno}`;
         /* console.log(
