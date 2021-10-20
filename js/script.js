ee84f2618fa424af376b81d0e2186fefca30bbf5
Andrea Scarpa <scarpandrea.1996@hotmail.com>
2021-10-20 15:51:49 +0200

    Impostazione esercizio ( commenti javascript)

---
+++ js/script.js
@@ -0,0 +1,15 @@
+/* Chiedi all’utente il suo nome,
+ poi chiedi il suo cognome,
+ poi chiedi il suo colore preferito
+Infine scrivi sulla pagina “La password assegnata è: ” nomecognomecolorepreferito21  */
+
+
+    // Chiediamo all utente il suo nome 
+    
+    // chiediamo all utente il suo cognome 
+    
+    // Chiediamo all utente il suo colore preferito
+
+    // definiamo un valore ipotetico (finale anno)
+
+    // Restituiamo all utente una possibile passsword generata 
